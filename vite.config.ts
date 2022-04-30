7933536114e57e5d2f3c7c69b2691cfcd8e43b16
john-mirage <chaosdynamix@posteo.net>
2022-04-30 15:59:17 +0200

    feat: deploy without tsc check

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/intro-section-with-dropdown/",
   build: {
     lib: {
       entry: 'src/web-bar.ts',
